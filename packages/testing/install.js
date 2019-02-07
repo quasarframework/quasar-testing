8b2360847b84574ca8c22223993ad33d1de3194d
Noah Klayman <noahklayman@gmail.com>
2019-02-07 11:29:19 -0800

    feat(testing): ask to install other harnesses during install

--- packages/testing/install.js
+++ packages/testing/install.js
@@ -5,19 +5,30 @@
  *
  */
 
-module.exports = function (api) {
+const execa = require('execa')
+
+module.exports = async function(api) {
   api.compatibleWithQuasarApp('^1.0.0-alpha.12')
 
   api.render('./base', {}, true)
-  /*
-  api.prompts.options.forEach((val) => {
-    if (val === 'scripts') {
-      api.extendPackageJson({
-        scripts: {
-          "test": "echo 'Testing managed by @Quasar/Testing' && exit 0"
+
+  for (const harness of api.prompts.harnesses) {
+    try {
+      await execa(
+        'quasar',
+        [
+          'ext',
+          '--add',
+          `@quasar/${harness}`,
+          process.argv.indexOf('--skip-pkg') > -1 ? '--skip-pkg' : ''
+        ],
+        {
+          stdio: 'inherit',
+          cwd: api.resolve.app('.')
         }
-      })
+      )
+    } catch (e) {
+      console.error(`Extension ${harness} failed to install:`, e)
     }
-  })
-  */
+  }
 }
