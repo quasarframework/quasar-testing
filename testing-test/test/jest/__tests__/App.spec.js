128417075399314f597c45684ad63e702005937f
Noah Klayman <noahklayman@gmail.com>
2019-02-14 11:05:50 -0800

    fix(jest): demo test not passing eslint, fixes #39

--- testing-test/test/jest/__tests__/App.spec.js
+++ testing-test/test/jest/__tests__/App.spec.js
@@ -1,15 +1,14 @@
-/* eslint-disable */
 /**
  * @jest-environment jsdom
  */
 
-import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import { mountQuasar } from '~/test/jest/utils'
-import QBUTTON from './demo/QBtn-demo.vue'
-import Quasar, { Qbtn } from 'quasar'
+import QButton from './demo/QBtn-demo.vue'
+import Quasar from 'quasar'
 
 describe('Mount Quasar', () => {
-  const wrapper = mountQuasar(QBUTTON, {
+  const wrapper = mountQuasar(QButton, {
     utils: {
       appError: () => (fn) => fn,
       appSuccess: () => (fn) => fn
@@ -33,14 +32,14 @@ describe('Mount Quasar', () => {
 
   it('sets the correct default data', () => {
     expect(typeof vm.counter).toBe('number')
-    const defaultData2 = QBUTTON.data()
+    const defaultData2 = QButton.data()
     expect(defaultData2.counter).toBe(0)
   })
 
   it('correctly updates data when button is pressed', () => {
     const localVue = createLocalVue()
     localVue.use(Quasar, { components: ['QBtn']})
-    const wrapper2 = mount(QBUTTON, {
+    const wrapper2 = mount(QButton, {
       localVue
     })
     const vm2 = wrapper2.vm
