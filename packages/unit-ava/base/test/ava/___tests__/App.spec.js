87d7d571099c0b0d2e5619f775fc972375dc2f7d
Noah Klayman <noahklayman@gmail.com>
2019-02-14 10:49:23 -0800

    feat(ava): provide better demo test (from jest), fixes #37 (#42)

--- packages/unit-ava/base/test/ava/___tests__/App.spec.js
+++ packages/unit-ava/base/test/ava/___tests__/App.spec.js
@@ -1,13 +1,45 @@
+import { mount, createLocalVue } from '@vue/test-utils'
 import { mountQuasar } from '../utils'
-import App from 'src/App.vue'
+import QButton from './demo/QBtn-demo.vue'
+import Quasar from 'quasar'
 import test from 'ava'
 
-test('Inits component', t => {
-  const component = mountQuasar(App, {
-    utils: {
-      appError: () => fn => fn,
-      appSuccess: () => fn => fn
-    }
+const wrapper = mountQuasar(QButton, {
+  utils: {
+    appError: () => fn => fn,
+    appSuccess: () => fn => fn
+  }
+})
+const vm = wrapper.vm
+
+test('passes the sanity check and creates a wrapper', t => {
+  t.true(wrapper.isVueInstance())
+})
+
+test('has a created hook', t => {
+  t.is(typeof vm.increment, 'function')
+})
+
+test('accesses the shallowMount', t => {
+  t.regex(vm.$el.textContent, /rocket muffin/)
+  t.regex(wrapper.text(), /rocket muffin/) // easier
+  t.regex(wrapper.find('p').text(), /rocket muffin/)
+})
+
+test('sets the correct default data', t => {
+  t.is(typeof vm.counter, 'number')
+  const defaultData2 = QButton.data()
+  t.is(defaultData2.counter, 0)
+})
+
+test('correctly updates data when button is pressed', t => {
+  const localVue = createLocalVue()
+  localVue.use(Quasar, { components: ['QBtn'] })
+  const wrapper2 = mount(QButton, {
+    localVue
   })
-  t.true(component.exists())
+  const vm2 = wrapper2.vm
+  const button = wrapper2.find('button')
+  button.trigger('click')
+  t.is(vm2.counter, 1)
 })
