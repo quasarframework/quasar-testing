a2140216b31f587e9834befb51256090faa53812
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-02-15 10:28:05 +0100

    Create base package for security testing
    Signed-off-by: Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>

---
+++ packages/security/install.js
@@ -0,0 +1,18 @@
+/**
+ * Quasar App Extension install script
+ *
+ * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
+ *
+ */
+
+module.exports = function (api) {
+  api.render('./base', {}, true)
+  api.prompts.options.forEach((val) => {
+    if (val === 'scripts') {
+      api.extendPackageJson({
+        scripts: {
+        }
+      })
+    }
+  })
+}
