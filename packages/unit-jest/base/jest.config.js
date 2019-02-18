2311b458682ab08ecc3b56dae08b075485169e8d
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-02-18 17:13:58 +0100

    feat(jest) Use proper transform, retain workaround as comment.
    Signed-off-by: Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>

--- packages/unit-jest/base/jest.config.js
+++ packages/unit-jest/base/jest.config.js
@@ -47,8 +47,11 @@ module.exports = {
     '.*css$': '<rootDir>/test/jest/utils/stub.css'
   },
   transform: {
-    '.*\\.vue$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/vue-jest',
-    '.*\\.js$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/babel-jest'
+    '.*\\.vue$': 'vue-jest',
+    '.*\\.js$': 'babel-jest'
+    // use these if NPM is being flaky
+    // '.*\\.vue$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/vue-jest',
+    // '.*\\.js$': '<rootDir>/node_modules/@quasar/quasar-app-extension-testing-unit-jest/node_modules/babel-jest'
   },
   snapshotSerializers: [
     '<rootDir>/node_modules/jest-serializer-vue'
