a09ad069395d5fbfbf2f989e6ccdc038e956fb02
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-03-19 22:33:58 +0100

    feat(test app) update test app
    Signed-off-by: Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>

---
+++ testing-test/test/ava/helpers/setup.js
@@ -0,0 +1,27 @@
+// Setup browser environment
+require('jsdom-global')()
+// https://github.com/vuejs/vue-cli/issues/2128
+window.Date = Date
+const hooks = require('require-extension-hooks')
+const Vue = require('vue')
+
+// Setup Vue.js to remove production tip
+Vue.config.productionTip = false
+
+// Setup vue files to be processed by `require-extension-hooks-vue`
+hooks('vue')
+  .plugin('vue')
+  .push()
+// Setup vue and js files to be processed by `require-extension-hooks-babel`
+hooks(['vue', 'js'])
+  .exclude(({ filename }) => filename.match(/\/node_modules\//))
+  .plugin('babel')
+  .push()
+
+// Notify babel config of test
+process.env.QUASAR_APP_TEST = true
+
+// Error if console.log is called
+// console.log = () => {
+//  throw new Error('Do not use console.log() in production')
+// }
