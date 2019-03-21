6ca69d32d9017405611249b09ab1b45dcc552d15
Noah Klayman <noahklayman@gmail.com>
2019-03-21 11:39:04 -0700

    fix(unit-jest/template): import all components

--- packages/unit-jest/src/base/test/jest/___tests__/App.spec.js
+++ packages/unit-jest/src/base/test/jest/___tests__/App.spec.js
@@ -5,14 +5,20 @@
 
 import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
 import QBUTTON from './demo/QBtn-demo.vue'
-import { Quasar, QBtn, date } from 'quasar'
+import * as All from 'quasar'
 import langEn from 'quasar/lang/en-us' // change to any language you wish!
+const { Quasar, date } = All
 
+const components = Object.keys(All).reduce((object, key) => {
+  const val = All[key]
+  if (val && val.component && val.component.name != null) {
+    object[key] = val
+  }
+  return object
+}, {})
 describe('Mount Quasar', () => {
   const localVue = createLocalVue()
-  localVue.use(Quasar, { components: { QBtn },
-    lang: langEn
-  })
+  localVue.use(Quasar, { components, lang: langEn })
 
   const wrapper = mount(QBUTTON, {
     localVue
