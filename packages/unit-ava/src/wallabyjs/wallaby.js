f485edb5aaf9f61d99b53ed35900294a167bc33a
Noah Klayman <noahklayman@gmail.com>
2019-03-24 08:58:40 -0700

    fix(unit-ava): Wallaby.js tests failing

--- packages/unit-ava/src/wallabyjs/wallaby.js
+++ packages/unit-ava/src/wallabyjs/wallaby.js
@@ -1,19 +1,41 @@
-module.exports = (wallaby) => {
+module.exports = wallaby => {
+  const path = require('path')
   process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true
+  process.env.QUASAR_APP_TEST = true
 
-  const compiler = wallaby.compilers.babel({ presets: [['@quasar/app', { modules: 'commonjs' }]] })
+  const compiler = wallaby.compilers.babel({
+    presets: [['@quasar/app', { presetEnv: { modules: 'commonjs' } }]],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            quasar: path.join(
+              wallaby.projectCacheDir,
+              'node_modules/quasar/dist/quasar.umd.js'
+            ),
+            'test-utils': path.join(
+              wallaby.projectCacheDir,
+              'node_modules/@vue/test-utils'
+            ),
+            '~': path.join(wallaby.projectCacheDir, './')
+          }
+        }
+      ]
+    ]
+  })
 
   return {
     files: [
       'src/**/*',
-      'jest.config.js',
+      'ava.config.js',
       'package.json',
       'test/**/*',
       '!test/**/*.spec.js',
       '!src/**/*.spec.js'
     ],
 
-    tests: ['src/**/*_spec.js', 'test/jest/**/*.spec.js'],
+    tests: ['src/**/*_spec.js', 'test/ava/**/*.spec.js'],
 
     env: {
       type: 'node',
@@ -27,6 +49,15 @@ module.exports = (wallaby) => {
 
     preprocessors: {
       '**/*.vue': file => require('vue-jest').process(file.content, file.path)
+    },
+    setup: () => {
+      require('jsdom-global')(undefined, {
+        url: 'http://localhost'
+      })
+      // https://github.com/vuejs/vue-cli/issues/2128
+      window.Date = Date
+      // https://github.com/vuejs/vue/issues/9698
+      global.performance = window.performance
     },
     testFramework: 'ava',
     debug: true
