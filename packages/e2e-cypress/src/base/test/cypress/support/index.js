aca628ca138420475cf0802caa81898133eaa61b
Arjon Jason Castro <ajcastro29@gmail.com>
2019-05-28 16:36:16 +0800

    Put the missing err parameter in the callback handler causing errors in test (#88)

--- packages/e2e-cypress/src/base/test/cypress/support/index.js
+++ packages/e2e-cypress/src/base/test/cypress/support/index.js
@@ -18,7 +18,7 @@ import './commands'
 
 const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
 
-Cypress.on('uncaught:exception', () => {
+Cypress.on('uncaught:exception', (err) => {
   if (resizeObserverLoopErrRe.test(err.message)) {
     // returning false here prevents Cypress from
     // failing the test
