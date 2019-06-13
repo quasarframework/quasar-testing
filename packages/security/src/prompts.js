e7ef343e0e69100208041af4ee6ce65780957866
Daniel Thompson-Yvetot <35242872+nothingismagick@users.noreply.github.com>
2019-06-13 23:43:46 +0200

    chore(cleanup) zap

--- packages/security/src/prompts.js
+++ packages/security/src/prompts.js
@@ -7,8 +7,8 @@ module.exports = function () {
       message: 'Please choose the following options:',
       choices: [
         {
-		      name: 'install the OWASP ZAP Heads Up Display',
-		      value: 'zap_local',
+          name: 'install the OWASP ZAP Heads Up Display',
+          value: 'zap_local',
         },
         {
           name: 'use your Global ZAP (not yet available)',
@@ -16,22 +16,22 @@ module.exports = function () {
         }
       ]
     },
-	  {
-		  name: 'zapbrowser',
-		  type: 'list',
-		  required: true,
-		  message: 'Please choose the browser to launch for ZAP testing:',
-		  choices: [
-			  {
-				  name: 'Firefox',
-				  value: 'Firefox'
-			  },
-			  {
-				  name: 'Chrome',
-				  value: 'Chrome'
-			  }
-		  ],
-		  default: 'Firefox'
-	  }
+    {
+      name: 'zapbrowser',
+      type: 'list',
+      required: true,
+      message: 'Please choose the browser to launch for ZAP testing:',
+      choices: [
+        {
+          name: 'Firefox',
+          value: 'Firefox'
+        },
+        {
+          name: 'Chrome',
+          value: 'Chrome'
+        }
+      ],
+      default: 'Firefox'
+    }
   ]
 }
