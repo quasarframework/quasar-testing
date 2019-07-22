9974840a2f9d8e19091323f54d9e60ef2f8e7934
webnoob <allangaunt@gmail.com>
2019-07-22 15:50:44 +0100

    fix (AV) - Add message for coming soon as we're moving to internal project.

--- packages/security-antivuln/src/index.js
+++ packages/security-antivuln/src/index.js
@@ -5,25 +5,6 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-const AntiVuln = require('./AntiVuln')
-
 module.exports = async function (api) {
-  try {
-    const antiVuln = new AntiVuln(api)
-    if (
-      (api.ctx.dev && api.prompts.options.includes('runOnDev')) ||
-      (api.ctx.prod && api.prompts.options.includes('runOnBuild'))
-    ) {
-      let result = await antiVuln.run()
-      if (result !== void 0) {
-        if (api.prompts.options.includes('strict')) {
-          console.log('Bad dependencies found; exiting.')
-          process.exit(0)
-        }
-      }
-    }
-  } catch (e) {
-    console.log(e)
-    process.exit(1)
-  }
+  console.log('    AntiVuln - Coming soon! https://antivuln.dev')
 }
