e511df858d2f584e7d2ffb27055dc2963dfa4e30
Carsten Walther <github@carstenwalther.io>
2019-10-10 11:05:48 +0200

    Remove obsolete bug workaround
    The bug is fixed since cypress 3.0.3 according to https://github.com/cypress-io/cypress/issues/1620

--- packages/e2e-cypress/src/base/test/cypress/plugins/index.js
+++ packages/e2e-cypress/src/base/test/cypress/plugins/index.js
@@ -17,14 +17,4 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   // console.log(config); // see what all is in here!
-
-
-  // Chrome:: Hack for shaking AUT. Cypress Issue: https://github.com/cypress-io/cypress/issues/1620
-  on('before:browser:launch', (browser = {}, args) => {
-    if (browser.name === 'chrome') {
-      args.push('--disable-blink-features=RootLayerScrolling');
-      return args;
-    }
-    return true;
-  });
 };
