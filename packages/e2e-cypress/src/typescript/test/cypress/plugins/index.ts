c4e2c8254be86e53511d52960da0585520c1fa79
Paolo Caleffi <p.caleffi@dreamonkey.com>
2020-06-12 15:12:48 +0200

    feat(cypress-ts): use TS files for support files too

---
+++ packages/e2e-cypress/src/typescript/test/cypress/plugins/index.ts
@@ -0,0 +1,21 @@
+/* eslint-env node */
+// ***********************************************************
+// This example plugins/index.ts can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+// cypress/plugins/index.ts
+
+const pluginConfig: Cypress.PluginConfig = (/*on, config*/) => {
+  //
+};
+
+export default pluginConfig;
