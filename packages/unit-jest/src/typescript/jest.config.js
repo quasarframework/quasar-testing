51dd6315e34492c89c064610ac236166615ffe16
Jeremiah <i3lfrg@gmail.com>
2020-09-20 14:17:18 -0600

    chore(jest): disable coverage by default
    test:unit:coverage already exists and still works

--- packages/unit-jest/src/typescript/jest.config.js
+++ packages/unit-jest/src/typescript/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // cache: false,
   // verbose: true,
   // watch: true,
-  collectCoverage: true,
+  collectCoverage: false,
   coverageDirectory: '<rootDir>/test/jest/coverage',
   collectCoverageFrom: [
     '<rootDir>/src/**/*.vue',
