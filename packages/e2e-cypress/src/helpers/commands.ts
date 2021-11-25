9d6cb24a45fea111dc8aed8c6507bf53132bed3f
Evert van der Weit <evertvanderweit@gmail.com>
2021-11-25 20:26:54 +0100

    feat(testing): add more commands and some custom assertions - dataCy enhancements (pass options to cy.get()) - add vue command to get vueWrapper - add two assertions to simply test colors

--- packages/e2e-cypress/src/helpers/commands.ts
+++ packages/e2e-cypress/src/helpers/commands.ts
@@ -1,11 +1,48 @@
+import { VueWrapper } from '@vue/test-utils';
+
 declare global {
   namespace Cypress {
+    interface Chainer<Subject> {
+      /**
+       * @example
+       *    cy.get('foo').should('have.color', 'white')
+       *    cy.get('foo').should('have.color', '#fff')
+       *    cy.get('foo').should('have.color', 'var(--q-primary)')
+       */
+      (chainer: 'have.color', type: string): Chainable<Subject>;
+      /**
+       * @example
+       *    cy.get('foo').should('have.backgroundColor', 'black')
+       *    cy.get('foo').should('have.backgroundColor', '#000')
+       *    cy.get('foo').should('have.backgroundColor', 'var(--q-dark)')
+       */
+      (chainer: 'have.backgroundColor', type: string): Chainable<Subject>;
+    }
     interface Chainable {
       /**
        * Custom command to select DOM element by data-cy attribute.
        * @example cy.dataCy('greeting')
+       * @example cy.dataCy('greeting', { timeout: 0 })
        */
-      dataCy<E extends Node = HTMLElement>(value: string): Chainable<JQuery<E>>;
+      dataCy<E extends Node = HTMLElement>(
+        value: string,
+        options?: Partial<
+          Cypress.Loggable &
+            Cypress.Timeoutable &
+            Cypress.Withinable &
+            Cypress.Shadow
+        >,
+      ): Chainable<JQuery<E>>;
+
+      /**
+       * Custom command to get the vue wrapper from a cypress instance.
+       * @example cy.dataCy('greeting')
+       *            .vue()
+       *            .then((wrapper) => {
+       *                wrapper.
+       *            })
+       */
+      vue(): Chainable<VueWrapper<any>>;
 
       /**
        * Custom command to test being on a given route.
@@ -32,12 +69,32 @@ export const registerCommands = () => {
   Cypress.Commands.add(
     'dataCy',
     { prevSubject: 'optional' },
-    (subject: JQuery<HTMLElement> | undefined, value: string) => {
-      return cy.get(`[data-cy=${value}]`, {
-        withinSubject: subject,
-      });
+    (
+      subject: JQuery<HTMLElement> | undefined,
+      value: string,
+      options?: Partial<
+        Cypress.Loggable &
+          Cypress.Timeoutable &
+          Cypress.Withinable &
+          Cypress.Shadow
+      >,
+    ) => {
+      return cy.get(
+        `[data-cy=${value}]`,
+        Object.assign(
+          {
+            withinSubject: subject,
+          },
+          options,
+        ),
+      );
     },
   );
+
+  Cypress.Commands.add('vue', () => {
+    // @ts-expect-error // This is not typed by Cypress
+    return cy.wrap(Cypress.vueWrapper);
+  });
 
   Cypress.Commands.add('testRoute', (route: string) => {
     cy.location().should((loc) => {
@@ -53,6 +110,44 @@ export const registerCommands = () => {
       ).to.be.true;
     });
   });
+
+  // This is a helper function to convert assert colors correctly
+  // Cypress looks at the computed color which is always rgb()
+  // This makes it possible to compare `black` to `rgb(0, 0, 0)` for instance
+  const compareColor =
+    (color: string, property: string) =>
+    (targetElement: JQuery<HTMLElement>) => {
+      const tempElement = document.createElement('div');
+      tempElement.style.color = color;
+      tempElement.style.display = 'none'; // make sure it doesn't actually render
+      document.body.appendChild(tempElement); // append so that `getComputedStyle` actually works
+
+      const tempColor = getComputedStyle(tempElement).color;
+      // @ts-expect-error
+      const targetColor = getComputedStyle(targetElement[0])[property];
+
+      document.body.removeChild(tempElement); // remove it because we're done with it
+
+      expect(tempColor).to.equal(targetColor);
+    };
+
+  // By default the `have.css` matcher will compare the computedColor, which is always a rgb() value.
+  // This creates a custom matcher that you can pass any color and it will compute a rgb() value from it.
+  Cypress.Commands.overwrite(
+    'should',
+    (originalFn, subject, expectation, ...args) => {
+      const customMatchers: Record<string, any> = {
+        'have.backgroundColor': compareColor(args[0], 'backgroundColor'),
+        'have.color': compareColor(args[0], 'color'),
+      };
+
+      // See if the expectation is a string and if it is a member of Jest's expect
+      if (typeof expectation === 'string' && customMatchers[expectation]) {
+        return originalFn(subject, customMatchers[expectation]);
+      }
+      return originalFn(subject, expectation, ...args);
+    },
+  );
 
   // these two commands let you persist local storage between tests
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
