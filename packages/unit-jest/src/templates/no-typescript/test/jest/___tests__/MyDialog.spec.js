32805ed461bfa0701041df3f9bdb8409433fc62e
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-01-18 12:44:17 +0100

    fix(jest): fix wrong import into example test

--- packages/unit-jest/src/templates/no-typescript/test/jest/___tests__/MyDialog.spec.js
+++ packages/unit-jest/src/templates/no-typescript/test/jest/___tests__/MyDialog.spec.js
@@ -1,13 +1,13 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
 import { describe, expect, it } from '@jest/globals';
 import { mount } from '@vue/test-utils';
-import CustomDialog from 'components/CustomDialog';
+import MyDialog from './demo/MyDialog';
 
 installQuasarPlugin();
 
 describe('MyDialog', () => {
   it('should mount MyDialog', () => {
-    const wrapper = mount(CustomDialog, {
+    const wrapper = mount(MyDialog, {
       data: () => ({
         isDialogOpen: true,
       }),
