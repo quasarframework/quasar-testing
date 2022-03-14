ece7f3bb85b61e8975c107270afe243b583268d3
Dean Robinson <dean.robinson@icloud.com>
2022-03-14 16:15:51 +0000

    add example test for portal based component typescript folder

--- packages/unit-jest/src/templates/typescript/test/jest/___tests__/MyDialog.spec.js
+++ packages/unit-jest/src/templates/typescript/test/jest/___tests__/MyDialog.spec.js
@@ -1,18 +1,31 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-jest';
-import { describe, expect, it } from '@jest/globals';
-import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, it } from '@jest/globals';
+import { DOMWrapper, mount } from '@vue/test-utils';
 import MyDialog from './demo/MyDialog';
 
 installQuasarPlugin();
 
+document.body.innerHTML = `
+  <div>
+    <div id="app"></div>
+  </div>
+`;
+
 describe('MyDialog', () => {
-  it('should mount MyDialog', () => {
-    const wrapper = mount(MyDialog, {
+  beforeEach(() => {
+    mount(MyDialog, {
+      attachTo: document.getElementById('app'),
       data: () => ({
         isDialogOpen: true,
       }),
     });
+  });
 
-    expect(wrapper.exists()).toBe(true);
+  it('should attach the dialog to the document body and allow for testing', () => {
+    const documentWrapper = new DOMWrapper(document.body);
+    const dialog = documentWrapper.find('.q-dialog');
+
+    expect(dialog.exists()).toBeTruthy();
+    expect(dialog.html()).toContain('Custom dialog which should be tested');
   });
 });
