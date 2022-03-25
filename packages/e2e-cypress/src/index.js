fd15e4e394a1dd31af550ee8e5bfdfc8bee1d9f2
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-03-25 16:46:21 +0100

    fix(testing): manage app-webpack/app-vite change

--- packages/e2e-cypress/src/index.js
+++ packages/e2e-cypress/src/index.js
@@ -6,17 +6,17 @@
  */
 
 module.exports = function (api) {
+  api.compatibleWith('quasar', '^2.0.0');
+
+  if (api.hasVite) {
+    api.compatibleWith('@quasar/app-vite', '^1.0.0-beta.8');
+  } else if (api.hasWebpack) {
+    // TODO: should be "@quasar/app-webpack" but that is not backward compatible
+    // Remove when Qv3 comes out
+    api.compatibleWith('@quasar/app', '^3.0.0');
+  }
+
   api.extendQuasarConf((conf) => {
-    api.compatibleWith('quasar', '^2.0.0');
-
-    if (api.hasVite) {
-      api.compatibleWith('@quasar/app-vite', '^1.0.0-beta.8');
-    } else if (api.hasWebpack) {
-      // TODO: should be "@quasar/app-webpack" but that is not backward compatible
-      // Remove when Qv3 comes out
-      api.compatibleWith('@quasar/app', '^3.0.0');
-    }
-
     // TODO: we should remove this in next AE major version, since Cypress 9.1 set a process.env.CYPRESS variable for us
     // See https://github.com/cypress-io/cypress/issues/18805
     if (process.env.E2E_TEST) {
--- packages/testing/src/index.js
+++ packages/testing/src/index.js
@@ -1,4 +1,14 @@
 module.exports = async function (api) {
+  api.compatibleWith('quasar', '^2.0.0');
+
+  if (api.hasVite) {
+    api.compatibleWith('@quasar/app-vite', '^1.0.0-beta.8');
+  } else if (api.hasWebpack) {
+    // TODO: should be "@quasar/app-webpack" but that is not backward compatible
+    // Remove when Qv3 comes out
+    api.compatibleWith('@quasar/app', '^3.0.0');
+  }
+
   // TODO async handler
   api.registerCommand('test', async ({ args: rawArgs, params: args }) => {
     const execa = require('execa');
