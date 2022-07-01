2a97003f26085d00aa42be8fd8b4fbcb8cfac447
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-07-01 15:10:53 +0200

    feat(cypress): update templates

--- packages/e2e-cypress/src/templates/no-typescript/src/components/___tests__/QuasarSelect.spec.ts
+++ packages/e2e-cypress/src/templates/no-typescript/src/components/___tests__/QuasarSelect.spec.ts
@@ -1,26 +1,31 @@
 import { mount } from '@cypress/vue';
 import QuasarSelect from '../QuasarSelect.vue';
 
+function dataCySelect(dataCyId) {
+  return cy.dataCy(dataCyId).closest('.q-select');
+}
+
 describe('QuasarSelect', () => {
+  it('makes sure the select is disabled', () => {
+    mount(QuasarSelect, {
+      props: { disable: true },
+    });
+
+    // `cy.dataCy('select')` won't work in this case, as it won't get the root q-select element
+    dataCySelect('select').should('have.attr', 'aria-disabled', 'true');
+  });
+
   it('selects an option by content', () => {
     mount(QuasarSelect);
 
-    cy.dataCy('select').click();
-    cy.withinSelectMenu(() => {
-      cy.contains('Option 1').click();
-    });
-
+    cy.dataCy('select').select('Option 1');
     cy.dataCy('select-value').should('have.text', 'Option 1');
   });
 
   it('selects an option by cardinality', () => {
     mount(QuasarSelect);
 
-    cy.dataCy('select').click();
-    cy.withinSelectMenu(() => {
-      cy.get('.q-item').eq(1).click();
-    });
-
+    cy.dataCy('select').select(1);
     cy.dataCy('select-value').should('have.text', 'Option 2');
   });
 
@@ -34,11 +39,7 @@ describe('QuasarSelect', () => {
     // Wait for loading to complete
     cy.dataCy('select').get('.q-spinner').should('not.exist');
 
-    cy.dataCy('select').click();
-    cy.withinSelectMenu(() => {
-      cy.contains('Option 3').click();
-    });
-
+    cy.dataCy('select').select('Option 3');
     cy.dataCy('select-value').should('have.text', 'Option 3');
   });
 });
