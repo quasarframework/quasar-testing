e7eaa264afcd136709a8cfbddbb0541d7b58696f
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-07-01 14:07:46 +0200

    feat(cypress): add vModelAdapter helper

--- packages/e2e-cypress/src/helpers/index.ts
+++ packages/e2e-cypress/src/helpers/index.ts
@@ -2,6 +2,7 @@
 /// <reference types="@cypress/vue" />
 
 export * from './install-quasar-plugin';
+export * from './v-model-adapter';
 export * from './commands';
 
 import './cct-dev-server/shim';
---
+++ packages/e2e-cypress/src/helpers/v-model-adapter.ts
@@ -0,0 +1,25 @@
+import { Ref, watch } from 'vue';
+
+// VTU won't accept reactive v-model binding, this adapter manually
+// set the model prop each time a new value is emitted
+// See https://github.com/vuejs/test-utils/discussions/279
+
+// See https://github.com/vuejs/test-utils/issues/871
+export function vModelAdapter<T>(
+  modelRef: Ref<T>,
+  modelName = 'modelValue',
+  // Without this, TS will start complaining as it cannot understand the dynamic type of
+  // the returned object and match it with the component props this helper is applied to.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): any {
+  watch(modelRef, (value) =>
+    Cypress.vueWrapper.setProps({ [modelName]: value }),
+  );
+
+  return {
+    [modelName]: modelRef.value,
+    [`onUpdate:${modelName}`]: (emittedValue: T) => {
+      modelRef.value = emittedValue;
+    },
+  };
+}
