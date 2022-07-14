3d61515d6c5a79ca1e699622339ead83a261d022
Marc Pont <mpont91@gmail.com>
2022-07-14 08:50:39 +0200

    Add more layout injections

--- packages/unit-vitest/src/helpers/layout-injections.ts
+++ packages/unit-vitest/src/helpers/layout-injections.ts
@@ -1,3 +1,5 @@
+import { vi } from 'vitest';
+
 /**
  * Injections for Components with a QPage root Element
  */
@@ -11,7 +13,16 @@ export function qLayoutInjections() {
       right: { size: 300, offset: 0, space: false },
       footer: { size: 0, offset: 0, space: false },
       left: { size: 300, offset: 0, space: false },
-      isContainer: false,
+      isContainer: { value: false },
+      view: { value: 'lHh Lpr lff' },
+      rows: { value: { top: 'lHh', middle: 'Lpr', bottom: 'lff' } },
+      height: { value: 900 },
+      instances: {},
+      update: vi.fn(),
+      animate: vi.fn(),
+      totalWidth: { value: 1200 },
+      scroll: { value: { position: 0, direction: 'up' } },
+      scrollbarWidth: { value: 125 },
     },
   };
 }
---
+++ test-project-vite/test/vitest/__tests__/LayoutComponent.test.ts
@@ -0,0 +1,13 @@
+import { installQuasar } from '@quasar/quasar-app-extension-testing-unit-vitest';
+import { mount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+import LayoutComponent from './demo/LayoutComponent.vue';
+
+installQuasar();
+
+describe('layout example', () => {
+  it('should mount component properly', () => {
+    const wrapper = mount(LayoutComponent);
+    expect(wrapper.exists()).to.be.true;
+  });
+});
