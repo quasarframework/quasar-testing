7536ca24df8f092bfb3fbbacc85616b5142e8a05
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-10-12 15:32:59 +0200

    test(project-vite): upgrade to Cypress 10

--- test-project-vite/src/components/__tests__/QuasarTooltip.cy.ts
+++ test-project-vite/src/components/__tests__/QuasarTooltip.cy.ts
@@ -1,9 +1,8 @@
-import { mount } from '@cypress/vue';
 import QuasarTooltip from '../QuasarTooltip.vue';
 
 describe('QuasarTooltip', () => {
   it('should show a tooltip', () => {
-    mount(QuasarTooltip);
+    cy.mount(QuasarTooltip);
 
     cy.dataCy('button').trigger('mouseover');
     cy.dataCy('tooltip').contains('Here I am!');
