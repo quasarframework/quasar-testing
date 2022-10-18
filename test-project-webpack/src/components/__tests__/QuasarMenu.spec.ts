341e24bd0983ce03e3f0e0049db241458738a115
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-10-18 13:02:55 +0200

    fix(cypress): fix QMenu helper for Quasar > 2.8.4

---
+++ test-project-webpack/src/components/__tests__/QuasarMenu.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from '@cypress/vue';
+import QuasarMenu from '../QuasarMenu.vue';
+
+describe('QuasarMenu', () => {
+  it('click an item by content', () => {
+    mount(QuasarMenu);
+
+    cy.dataCy('open-menu-btn').click();
+    cy.withinMenu(() => {
+      cy.get('.q-item').contains('Item 1').click();
+    });
+  });
+
+  it('click an item by cardinality', () => {
+    mount(QuasarMenu);
+
+    cy.dataCy('open-menu-btn').click();
+    cy.withinMenu(() => {
+      cy.get('.q-item').eq(1).click();
+    });
+  });
+});
