7fc5f27bbc140c30276bf979b1636f0ea25eef3c
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-10-27 17:32:34 +0200

    test(app-vite): remove Jest AE as it's not compatible with Vite

--- test-project-vite/babel.config.js
+++ test-project-vite/babel.config.js
@@ -1,15 +1,4 @@
 /* eslint-disable */
-const fs = require('fs');
-let extend = undefined;
-
-/**
- * The .babelrc file has been created to assist Jest for transpiling.
- * You should keep your application's babel rules in this file.
- */
-
-if (fs.existsSync('./.babelrc')) {
-  extend = './.babelrc';
-}
 
 module.exports = (api) => {
   return {
@@ -21,6 +10,5 @@ module.exports = (api) => {
           : {},
       ],
     ],
-    extends: extend,
   };
 };
