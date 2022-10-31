d652d0242156485803e59844477e7c1db66bd9ce
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-10-31 18:08:32 +0100

    feat(jest): align layout injections to Vitest ones

--- packages/unit-jest/src/helpers/layout-injections.ts
+++ packages/unit-jest/src/helpers/layout-injections.ts
@@ -1,3 +1,8 @@
+// TODO: we need the injection keys to be typed and exported from Quasar package
+
+import { jest } from '@jest/globals';
+import { ref } from 'vue';
+
 /**
  * Injections for Components with a QPage root Element
  */
@@ -7,11 +12,20 @@ export function qLayoutInjections() {
     _q_pc_: true,
     // layoutKey
     _q_l_: {
-      header: {},
-      right: {},
-      footer: {},
-      left: {},
-      isContainer: { value: false },
+      header: { size: 0, offset: 0, space: false },
+      right: { size: 300, offset: 0, space: false },
+      footer: { size: 0, offset: 0, space: false },
+      left: { size: 300, offset: 0, space: false },
+      isContainer: ref(false),
+      view: ref('lHh Lpr lff'),
+      rows: ref({ top: 'lHh', middle: 'Lpr', bottom: 'lff' }),
+      height: ref(900),
+      instances: {},
+      update: jest.fn(),
+      animate: jest.fn(),
+      totalWidth: ref(1200),
+      scroll: ref({ position: 0, direction: 'up' }),
+      scrollbarWidth: ref(125),
     },
   };
 }
