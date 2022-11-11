38805321d6a1ed18fb674df2d48a50d8d6643ead
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-11-11 12:20:31 +0100

    fix(jest): adapt Dialog test to Compositon API

--- packages/unit-jest/src/templates/no-typescript/test/jest/___tests__/MyDialog.spec.js
+++ packages/unit-jest/src/templates/no-typescript/test/jest/___tests__/MyDialog.spec.js
@@ -6,24 +6,29 @@ import MyDialog from './demo/MyDialog';
 installQuasarPlugin();
 
 describe('MyDialog', () => {
+  let wrapper;
+
   beforeEach(() => {
-    mount(MyDialog, {
-      data: () => ({
-        isDialogOpen: true,
-      }),
-    });
+    wrapper = mount(MyDialog);
+    // Always use `wrapper.vm.myProperty` to access properties
+    // returned by setup() or defined into <script setup>
+    // See https://test-utils.vuejs.org/api/#vm
+    // See https://github.com/vuejs/test-utils/issues/1770#issuecomment-1245751892
+    wrapper.vm.isDialogOpen = true;
   });
 
   it('should mount the document body and expose for testing', () => {
-    const wrapper = new DOMWrapper(document.body);
+    const bodyWrapper = new DOMWrapper(document.body);
 
-    expect(wrapper.find('.q-dialog').exists()).toBeTruthy();
+    // `.find()` won't error out even if the element is not found
+    expect(bodyWrapper.find('.q-dialog').exists()).toBeTruthy();
   });
 
   it('can check the inner text of the dialog', () => {
-    const wrapper = new DOMWrapper(document.body);
+    const bodyWrapper = new DOMWrapper(document.body);
 
-    expect(wrapper.find('.q-dialog').html()).toContain(
+    // `.get()` expects the element to exist, otherwise it throws an error
+    expect(bodyWrapper.get('.q-dialog').html()).toContain(
       'Custom dialog which should be tested',
     );
   });
