3ffb5f354c4549c2a54e8ca7c163025db25e7b29
Paolo Caleffi <p.caleffi@dreamonkey.com>
2022-11-11 14:20:38 +0100

    feat(testing): upgrade testing AE prompts

--- packages/testing/src/prompts.js
+++ packages/testing/src/prompts.js
@@ -17,16 +17,20 @@ module.exports = function () {
         'Please choose which testing harnesses to install (only Qv2-compatible harnesses are shown):',
       choices: [
         {
-          name: 'Jest Unit Testing',
+          name: 'Jest Unit Testing (Webpack only, beta)',
           value: 'unit-jest@beta',
         },
         {
-          name: 'Vitest Unit Testing',
+          name: 'Vitest Unit Testing (Vite only, alpha)',
           value: 'unit-vitest@alpha',
         },
         {
-          name: 'Cypress e2e Testing',
+          name: 'Cypress 9 e2e and component testing',
           value: 'e2e-cypress',
+        },
+        {
+          name: 'Cypress 10 e2e and component testing (beta)',
+          value: 'e2e-cypress@beta',
         },
       ],
     },
