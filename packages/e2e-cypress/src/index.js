2758100e46ad9e498630d5dc1fdea2b19d084ad6
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2022-11-17 13:40:15 +0300

    refactor(cypress): Drop redundant ternary usage

--- packages/e2e-cypress/src/index.js
+++ packages/e2e-cypress/src/index.js
@@ -34,7 +34,7 @@ module.exports = async function (api) {
       api.extendViteConf((viteConf) => {
         viteConf.plugins.push(
           istanbul({
-            forceBuildInstrument: api.ctx.prod ? true : false,
+            forceBuildInstrument: api.ctx.prod,
           }),
         );
       });
