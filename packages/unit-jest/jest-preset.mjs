7e9701bd5243fc481fdf45a48a780d56aa6dcb46
Kaveh Karami <Kaveh.Karami.KK@gmail.com>
2023-05-16 19:28:10 +0330

    make .jest part optional in unit-jest testMatch configuration

--- packages/unit-jest/jest-preset.mjs
+++ packages/unit-jest/jest-preset.mjs
@@ -31,7 +31,7 @@ export const config = {
     // Matches tests in any subfolder of 'src' or into 'test/jest/__tests__'
     // Matches all files with extension 'js', 'jsx', 'ts' and 'tsx'
     '<rootDir>/test/jest/__tests__/**/*.(spec|test).+(ts|js)?(x)',
-    '<rootDir>/src/**/*.jest.(spec|test).+(ts|js)?(x)',
+    '<rootDir>/src/**/*?(.jest).(spec|test).+(ts|js)?(x)',
   ],
   // Extension-less imports of components are resolved to .ts files by TS,
   //  grating correct type-checking in test files.
