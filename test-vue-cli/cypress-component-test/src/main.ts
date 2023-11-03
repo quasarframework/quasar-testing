86ea227518bba5028d2f6f6db67d66a1ee81f6cd
Rogeriohsjr <rogerio.dasilva@formstack.com>
2023-11-03 14:36:54 -0400

    docs(vue-cli): add quasar using vue-cli command

--- test-vue-cli/cypress-component-test/src/main.ts
+++ test-vue-cli/cypress-component-test/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { Quasar } from 'quasar';
+import quasarUserOptions from './quasar-user-options';
 
-createApp(App).mount('#app');
+createApp(App).use(Quasar, quasarUserOptions).mount('#app');
---
+++ test-vue-cli/cypress-component-test/src/quasar-user-options.js
@@ -0,0 +1,8 @@
+import './styles/quasar.sass';
+import '@quasar/extras/material-icons/material-icons.css';
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {},
+};
