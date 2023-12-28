042292265862b6a706df304d9b4841d9a33b0f00
Paolo Caleffi <p.caleffi@dreamonkey.com>
2023-12-28 17:56:04 +0100

    chore: bump minimum required versions for new q/app major releases

--- packages/unit-vitest/src/index.js
+++ packages/unit-vitest/src/index.js
@@ -7,5 +7,8 @@
 
 module.exports = function (api) {
   api.compatibleWith('quasar', '^2.6.0');
-  api.compatibleWith('@quasar/app-vite', '^1.0.0 || ^2.0.0-alpha.27');
+  // Vitest 1.0 requires Vite 5, which is only officially supported by @quasar/app-vite v2
+  // Yet, there are workarounds to make @quasar/app-vite@v1 work with Vite 5, so we aren't restricting this AE only to v2
+  // See https://github.com/quasarframework/quasar/issues/14077
+  api.compatibleWith('@quasar/app-vite', '^1.6.0 || ^2.0.0-alpha.44');
 };
--- packages/unit-vitest/src/install.js
+++ packages/unit-vitest/src/install.js
@@ -55,7 +55,10 @@ module.exports = async function (api) {
   api.compatibleWith('quasar', '^2.12.7');
   api.compatibleWith('vue', '^3.3.4');
   // hasTypescript is only available from v1.6.0 onwards
-  api.compatibleWith('@quasar/app-vite', '^1.6.0 || ^2.0.0-alpha.27');
+  // Vitest 1.0 requires Vite 5, which is only officially supported by @quasar/app-vite v2
+  // Yet, there are workarounds to make @quasar/app-vite@v1 work with Vite 5, so we aren't restricting this AE only to v2
+  // See https://github.com/quasarframework/quasar/issues/14077
+  api.compatibleWith('@quasar/app-vite', '^1.6.0 || ^2.0.0-alpha.44');
 
   api.render(`./templates/${await api.hasTypescript() ? '' : 'no-'}typescript`);
 
