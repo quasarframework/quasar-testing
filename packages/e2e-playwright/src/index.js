01a32e87314d2143bf21c25634f3b6c89e6cef6a
Calson Chiatiah <chiacalson@gmail.com>
2025-04-02 11:26:03 +0100

    feat(playwright): set compatible Quasar version and enforce server port

--- packages/e2e-playwright/src/index.js
+++ packages/e2e-playwright/src/index.js
@@ -1,1 +1,16 @@
-export default function (api) {}
+import { enforcedDevServerPort } from './shared.js';
+
+export default function (api) {
+  api.compatibleWith('quasar', '^2.0.0');
+
+  if (process.env.NODE_ENV !== 'test') {
+    return;
+  }
+
+  api.extendQuasarConf(async (conf) => {
+    // Let's stop Quasar from opening a browser by default
+    conf.devServer.open = false;
+
+    conf.devServer.port = api.prompts.port ?? enforcedDevServerPort;
+  });
+}
