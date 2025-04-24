6368eae61549ea6c1c3d88448bd4faaac9656faa
Paolo Caleffi <p.caleffi@dreamonkey.com>
2025-04-24 22:35:25 +0200

    feat: sync all test projects and regenerate webpack v4 one

--- test-webpack-app-v4/src/router/index.ts
+++ test-webpack-app-v4/src/router/index.ts
@@ -1,4 +1,4 @@
-import { route } from 'quasar/wrappers';
+import { defineRouter } from '#q-app/wrappers';
 import {
   createMemoryHistory,
   createRouter,
@@ -16,7 +16,7 @@ import routes from './routes';
  * with the Router instance.
  */
 
-export default route(function (/* { store, ssrContext } */) {
+export default defineRouter(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : process.env.VUE_ROUTER_MODE === 'history'
