b980e9ad64b1356ef63ed094f035b6866a823501
Paolo Caleffi <p.caleffi@dreamonkey.com>
2025-10-02 15:51:02 +0200

    feat(cypress): sync test projects with Cypress 15 support

--- test-webpack-app-v4/eslint.config.js
+++ test-webpack-app-v4/eslint.config.js
@@ -118,7 +118,7 @@ config.push({
     '@typescript-eslint/no-unsafe-call': 'error',
     '@typescript-eslint/no-unsafe-member-access': 'error',
     '@typescript-eslint/no-unsafe-return': 'error',
-  }
+  },
 });
 
 export default config;
