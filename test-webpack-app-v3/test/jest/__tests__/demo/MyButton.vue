a57ee427954056f1e93f88431afad14c2ddf4331
Paolo Caleffi <p.caleffi@dreamonkey.com>
2025-10-02 15:20:03 +0200

    test: remove test-webpack-app-v3 test project, as it's incompatible with the test system due to Cypress v15 conflicts with non-ESM-ready packages

--- test-webpack-app-v3/test/jest/__tests__/demo/MyButton.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" src="./MyButton.ts"></script>
-
-<template>
-  <div>
-    <p class="content">{{ input }}</p>
-    <span>{{ counter }}</span>
-    <q-btn class="button" @click="increment()"></q-btn>
-  </div>
-</template>
