86f3eca211f0874c8954106223cde45af4021dcd
Paolo Caleffi <p.caleffi@dreamonkey.com>
2025-10-02 15:20:03 +0200

    test: remove test-webpack-app-v3 test project, as it's incompatible with the test system due to Cypress v15 conflicts with non-ESM-ready packages

--- test-webpack-app-v3/test/jest/__tests__/demo/MyDialog.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" src="./MyDialog.ts"></script>
-
-<template>
-  <q-dialog v-model="isDialogOpen">
-    <q-card>
-      <q-card-section>Custom dialog which should be tested</q-card-section>
-    </q-card>
-  </q-dialog>
-</template>
