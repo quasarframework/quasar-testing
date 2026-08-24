e98eb80980395d13d56ca82a2f22f6342c7b7ed5
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2026-08-24 18:26:05 +0300

    chore: remove the webpack test app

--- test-webpack-app-v4/test/jest/__tests__/demo/MyDialog.ts
+++
@@ -1,9 +0,0 @@
-import { defineComponent, ref } from 'vue';
-
-export default defineComponent({
-  name: 'MyDialog',
-  setup() {
-    const isDialogOpen = ref(false);
-    return { isDialogOpen };
-  },
-});
